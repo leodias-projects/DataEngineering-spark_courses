989463c3d60c5ef2cc3eddf82a26e1de158cee19
Leonardo Dias da Rosa <leonardo.rosa@poatek.com>
2021-12-28 17:10:06 -0300

    changing mostpopularmovie code

--- SparkScalaCourse/src/main/scala/com/leo/spark/MostPopularMovieDataset.scala
+++ SparkScalaCourse/src/main/scala/com/leo/spark/MostPopularMovieDataset.scala
@@ -5,9 +5,27 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.{FloatType, IntegerType, StructType}
 
+import scala.io.{Codec, Source}
+
 object MostPopularMovieDataset {
 
   final case class movie(movieID: Int)
+
+  def loadMoviesNames(): Map[Int,String] = {
+
+    implicit val codec: Codec = Codec("ISO-8859-1")
+    var movieNames: Map[Int, String] = Map()
+    val lines = Source.fromFile("../data/ml-100k/u.item")
+    for (line <- lines.getLines()){
+      val fields = line.split('|')
+      if (fields.length > 1){
+        movieNames += (fields(0).toInt -> fields(1))
+      }
+    }
+    lines.close()
+
+    movieNames
+  }
 
   def main(args: Array[String]): Unit = {
 
@@ -22,6 +40,10 @@ object MostPopularMovieDataset {
       .appName("MostPopularMovieDataset")
       .master("local[*]")
       .getOrCreate()
+
+    // Load movie names
+
+    val names = spark.sparkContext.broadcast(loadMoviesNames())
 
     // Create a dataset schema
     val movieReview = new StructType()
@@ -40,7 +62,15 @@ object MostPopularMovieDataset {
     // Select Movie ID
     val movie = movieReviewDataset.groupBy("movieID" ).count().orderBy(asc("count"))
 
-    movie.show(movie.count.toInt)
+    val lookupName : Int => String = (movieID:Int) => {
+      names.value(movieID)
+    }
+
+    val nameUDF = udf(lookupName)
+
+    val moviesWithNames = movie.withColumn("movieTitle", nameUDF(col("movieID")))
+
+    moviesWithNames.collect().foreach(println)
 
     spark.stop()
   }
